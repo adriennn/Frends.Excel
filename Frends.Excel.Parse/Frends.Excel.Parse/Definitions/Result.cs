6d67b4f6b0bb20f3909583d6e365f316f8e8bc55
Jefim Borissov <jefim.borissov@hiq.fi>
2022-03-17 16:04:58 +0200

    Initial Frends.Excel.Parse implementation

---
+++ Frends.Excel.Parse/Frends.Excel.Parse/Definitions/Result.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Data;
+
+namespace Frends.Excel.Parse.Definitions;
+
+public class Result
+{
+    /// <summary>
+    /// False if conversion fails.
+    /// </summary>
+    [DefaultValue("false")]
+    public bool Success { get; set; }
+
+    /// <summary>
+    /// Exception message.
+    /// </summary>
+    [DefaultValue("")]
+    public string ErrorMessage { get; private set; }
+
+    /// <summary>
+    /// Parsed Excel data set.
+    /// </summary>
+    /// <returns>String</returns>
+    public DataSet DataSet { get; set; }
+
+    public Result(bool success, DataSet dataSet, string errorMessage)
+    {
+        Success = success;
+        DataSet = dataSet;
+        ErrorMessage = errorMessage;
+    }
+}
---
+++ Frends.Excel.Parse/Frends.Excel.Parse/Excel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Text;
+using ExcelDataReader;
+using Frends.Excel.Parse.Definitions;
+
+namespace Frends.Excel.Parse;
+
+public static class Excel
+{
+    /// <summary>
+    /// Converts Excel file to data set. [Documentation](https://github.com/FrendsPlatform/Frends.Excel/tree/main/Frends.Excel.Parse)
+    /// </summary>
+    /// <param name="input">Input configuration</param>
+    /// <param name="options">Input options</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Result containing the parsed Excel as data set.</returns>
+    /// <exception cref="Exception"></exception>
+    public static Result Parse(
+        [PropertyTab] Input input,
+        [PropertyTab] Options options,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            using (var stream = new FileStream(input.Path, FileMode.Open))
+            {
+                using (var excelReader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = excelReader.AsDataSet();
+                    return new Result(true, result, null);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            if (options.ThrowErrorOnFailure)
+            {
+                throw new Exception("Error while parsing Excel file", ex);
+            }
+
+            return new Result(false, null, $"Error while parsing Excel file: {ex}");
+        }
+    }
+}
