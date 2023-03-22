3b7a6224f7d7f56241796754cb6393b582abcf76
Riku Virtanen <virtanen.riku91@gmail.com>
2023-03-22 13:48:53 +0200

    PR review changes

--- Frends.Excel.ConvertToJSON/Frends.Excel.ConvertToJSON.Tests/ExcelConvertTests.cs
+++ Frends.Excel.ConvertToJSON/Frends.Excel.ConvertToJSON.Tests/ExcelConvertTests.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -28,7 +29,6 @@ public class ExcelConvertTests
         _input.Path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../../../TestData/");
         _options = new();
         _options.ReadOnlyWorkSheetWithName = "";
-        _options.JToken = false;
     }
 
     [Test]
@@ -37,8 +37,8 @@ public class ExcelConvertTests
         // Test converting all worksheets of xlsx file to JSON.
         _input.Path = Path.Combine(_input.Path, "ExcelTestInput1.xlsx");
         var result = Excel.ConvertToJSON(_input, _options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""ExcelTestInput1.xlsx"",""worksheets"":[{""name"":""Sheet1"",""rows"":[{ ""RowNumber"":1,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""},{ ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""Kanji 働""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""Summa""}]},{ ""RowNumber"":2,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""},{ ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""6""}]}]},{""name"":""OmituinenNimi"",""rows"":[{ ""RowNumber"":1,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Kissa kuva""},{ ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""1""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""2""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""3""}]},{ ""RowNumber"":15,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""}]},{ ""RowNumber"":16,""Cells"":[{ ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""}]}]}]}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""ExcelTestInput1.xlsx"",""worksheets"":[{""name"":""Sheet1"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""Kanji 働""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""Summa""}]},{""RowNumber"":2,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""6""}]}]},{""name"":""OmituinenNimi"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Kissa kuva""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""1""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""2""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""3""}]},{""RowNumber"":15,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""}]},{""RowNumber"":16,""Cells"":[{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""}]}]}]}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 
     [Test]
@@ -47,8 +47,8 @@ public class ExcelConvertTests
         // Test converting all worksheets of xls file to JSON.
         _input.Path = Path.Combine(_input.Path, "ExcelTestInput2.xls");
         var result = Excel.ConvertToJSON(_input, _options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""ExcelTestInput2.xls"",""worksheets"":[{""name"":""Sheet1"",""rows"":[{ ""RowNumber"":1,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""},{ ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""Kanji 働""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""Summa""}]},{ ""RowNumber"":2,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""},{ ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""6""}]}]},{""name"":""OmituinenNimi"",""rows"":[{ ""RowNumber"":1,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Kissa kuva""},{ ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""1""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""2""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""3""}]},{ ""RowNumber"":15,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""}]},{ ""RowNumber"":16,""Cells"":[{ ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""}]}]}]}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""ExcelTestInput2.xls"",""worksheets"":[{""name"":""Sheet1"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""Kanji 働""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""Summa""}]},{""RowNumber"":2,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""6""}]}]},{""name"":""OmituinenNimi"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Kissa kuva""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""1""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""2""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""3""}]},{""RowNumber"":15,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""}]},{""RowNumber"":16,""Cells"":[{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""}]}]}]}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 
     [Test]
@@ -58,8 +58,8 @@ public class ExcelConvertTests
         _input.Path = Path.Combine(_input.Path, "ExcelTestInput1.xlsx");
         _options.ReadOnlyWorkSheetWithName = "Sheet1";
         var result = Excel.ConvertToJSON(_input, _options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""ExcelTestInput1.xlsx"",""worksheet"":{""name"":""Sheet1"",""rows"":[{ ""RowNumber"":1,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""}, { ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""Kanji 働""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""Summa""}]},{ ""RowNumber"":2,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""}, { ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""6""}]}]}}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""ExcelTestInput1.xlsx"",""worksheet"":{""name"":""Sheet1"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""Kanji 働""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""Summa""}]},{""RowNumber"":2,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""6""}]}]}}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 
     [Test]
@@ -69,8 +69,8 @@ public class ExcelConvertTests
         _input.Path = Path.Combine(_input.Path, "ExcelTestInput2.xls");
         _options.ReadOnlyWorkSheetWithName = "Sheet1";
         var result = Excel.ConvertToJSON(_input, _options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""ExcelTestInput2.xls"",""worksheet"":{""name"":""Sheet1"",""rows"":[{ ""RowNumber"":1,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""}, { ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""Kanji 働""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""Summa""}]},{ ""RowNumber"":2,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""}, { ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""6""}]}]}}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""ExcelTestInput2.xls"",""worksheet"":{""name"":""Sheet1"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Foo""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""Bar""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""Kanji 働""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""Summa""}]},{""RowNumber"":2,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""6""}]}]}}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 
     [Test]
@@ -82,8 +82,8 @@ public class ExcelConvertTests
         _options.DateFormat = DateFormats.DDMMYYYY;
         _options.ShortDatePattern = false;
         var result = Excel.ConvertToJSON(_input, _options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""TestDateFormat.xlsx"",""worksheet"":{""name"":""Sheet2"",""rows"":[{ ""RowNumber"":1,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""25/12/2021 0:00:00""}, { ""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""25/02/2021 12:45:41""},{ ""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""12/05/2020 0:00:00""},{ ""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""30/12/2021 0:00:00""}]}]}}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""TestDateFormat.xlsx"",""worksheet"":{""name"":""Sheet2"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""25/12/2021 0:00:00""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""25/02/2021 12:45:41""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""12/05/2020 0:00:00""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""30/12/2021 0:00:00""}]}]}}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 
     [Test]
@@ -92,8 +92,8 @@ public class ExcelConvertTests
         var input = new Input { Path = Path.Combine(excelFilesDir, "TestQuotes.xlsx") };
         var options = new Options { ThrowErrorOnFailure = true };
         var result = Excel.ConvertToJSON(input, options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""TestQuotes.xlsx"",""worksheets"":[{""name"":""Quote \"" again"",""rows"":[{ ""RowNumber"":1,""Cells"":[{ ""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Hello \"" quote""}]}]}]}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""TestQuotes.xlsx"",""worksheets"":[{""name"":""Quote \"" again"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""Hello \"" quote""}]}]}]}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 
     [Test]
@@ -105,8 +105,8 @@ public class ExcelConvertTests
         _options.DateFormat = DateFormats.MMDDYYYY;
         _options.ShortDatePattern = false;
         var result = Excel.ConvertToJSON(_input, _options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""TestDateFormat.xls"",""worksheet"":{""name"":""Sheet1"",""rows"":[{""RowNumber"":1, ""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""}, {""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""4""}]},{""RowNumber"":2, ""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""12/12/2021 12:00:00AM""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""02/25/2021 12:45:41PM""}, {""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""05/12/2020 12:00:00AM""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""12/12/2021 12:00:00AM""}]}]}}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""TestDateFormat.xls"",""worksheet"":{""name"":""Sheet1"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""1""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""3""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""4""}]},{""RowNumber"":2,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""12/12/2021 12:00:00 AM""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""02/25/2021 12:45:41 PM""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""05/12/2020 12:00:00 AM""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""12/12/2021 12:00:00 AM""}]}]}}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 
     [Test]
@@ -118,8 +118,8 @@ public class ExcelConvertTests
         _options.DateFormat = DateFormats.YYYYMMDD;
         _options.ShortDatePattern = false;
         var result = Excel.ConvertToJSON(_input, _options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""TestDateFormat.xlsx"",""worksheet"":{""name"":""Sheet2"",""rows"":[{""RowNumber"":1, ""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""2021/12/25 0:00:00""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2021/02/25 12:45:41""}, {""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""2020/05/12 0:00:00""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""2021/12/30 0:00:00""}]}]}}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""TestDateFormat.xlsx"",""worksheet"":{""name"":""Sheet2"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""2021/12/25 0:00:00""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""2021/02/25 12:45:41""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""2020/05/12 0:00:00""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""2021/12/30 0:00:00""}]}]}}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 
     [Test]
@@ -131,7 +131,7 @@ public class ExcelConvertTests
         _options.DateFormat = DateFormats.DDMMYYYY;
         _options.ShortDatePattern = true;
         var result = Excel.ConvertToJSON(_input, _options, default);
-        var expectedResult = @"{""workbook"":{""workbook_name"":""TestDateFormat.xls"",""worksheet"":{""name"":""Sheet2"",""rows"":[{""RowNumber"":1, ""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""25/12/2021""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""25/02/2021""}, {""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""12/05/2020""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""30/12/2021""}]}]}}}";
-        Assert.That(Regex.Replace(result.JSON ?? "", @"[\s+]", ""), Does.StartWith(Regex.Replace(expectedResult.ToString(), @"[\s+]", "")));
+        var expectedResult = @"{""workbook"":{""workbook_name"":""TestDateFormat.xls"",""worksheet"":{""name"":""Sheet2"",""rows"":[{""RowNumber"":1,""Cells"":[{""ColumnName"":""A"",""ColumnIndex"":1,""ColumnValue"":""25/12/2021""},{""ColumnName"":""B"",""ColumnIndex"":2,""ColumnValue"":""25/02/2021""},{""ColumnName"":""C"",""ColumnIndex"":3,""ColumnValue"":""12/05/2020""},{""ColumnName"":""D"",""ColumnIndex"":4,""ColumnValue"":""30/12/2021""}]}]}}}";
+        Assert.AreEqual(expectedResult, result.JSON);
     }
 }
